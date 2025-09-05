b870612c517ea63a52939bb6a0d8900b16c3e275
Virgile <virgile.bodin@etu.univ-orleans.fr>
2025-09-05 20:35:58 +0200

    Creation des fichier initiaux du projet

---
+++ src/main/java/com/bibliotheque/gestionbibli/GestionbibliApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.gestionbibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbibliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionbibliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibliotheque/gestionbibli/GestionbibliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.gestionbibli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionbibliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
